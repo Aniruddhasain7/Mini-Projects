a790541abb6230c912a9b8a637d376258d358e02
Aniruddha Sain <aniruddhasain315@gmail.com>
2025-11-04 22:03:33 +0530

    Delete Autopassword.py

--- Autopassword.py
+++
@@ -1,9 +0,0 @@
-import random
-lower="abcdefghijklmnopqrstuvwxyz"
-upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-num="0123456789"
-symbols="@#&*_"
-all=lower+upper+num+symbols
-length=16
-password="".join(random.sample(all,length))
-print(password)
