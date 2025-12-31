d08452847c8f1db714d9c88bb44eb11cbe31c1bf
Aniruddha Sain <aniruddhasain315@gmail.com>
2025-12-31 13:50:17 +0530

    Delete RandomPasswordGenerator.py

--- RandomPasswordGenerator.py
+++
@@ -1,9 +0,0 @@
-import random
-lower="abcdefghijklmnopqrstuvwxyz"
-upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-num="0123456789"
-symbols="@#&%$"
-all=lower+upper+num+symbols
-length=8
-password="".join(random.sample(all,length))
-print(password)
